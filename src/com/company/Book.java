f88519817425cd20543ed5b289265b8e55fe3628
zp14 <249368830@qq.com>
2018-07-13 09:21:39 +0800

    11

---
+++ src/com/company/Book.java
@@ -0,0 +1,41 @@
+package com.company;
+
+public class Book {
+	private String title; // 书名
+	private String author; // 作者
+	private String TM;
+	private String press; // 出版社会
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getAuthor() {
+		return author;
+	}
+
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+
+	public String getTM() {
+		return TM;
+	}
+
+	public void setTM(String TM) {
+		this.TM = TM;
+	}
+
+	public String getPress() {
+		return press;
+	}
+
+	public void setPress(String press) {
+		this.press = press;
+	}
+
+
+}
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,81 +6,63 @@ import java.util.List;
 public class Main {
 
     private static List<Student> list = new ArrayList<>();
-
+    static double average=0;
+    static double min= 9999999;
+    static double max= -9999999;
+    static double sum=0;
     public static void main(String[] args) {
+        for (int i = 0; i < 11; i++)
+        {
+            Student student = new Student();
+            student.setName("曾鹏");
+            student.setAge(20);
+            student.setSex("男");
+            student.setStuNum("2018");
+            student.setGrade("大一");
+            student.setScore(i*10);
+            list.add(student);
+        }
 
-//        for (int i = 0; i < 10; i++) {
-//            Student student = new Student("zhanshan" + i, true, i);
-//            list.add(student);
-//        }
-//        list.remove(4);
-//        int average = 0;
-//        int min = 9999;
-//        int max = -1;
-//        int sum = 0;
-//        for (int i = 0; i < list.size(); i++) {
-//            if (min > list.get(i).getAge()) {
-//                min = list.get(i).getAge();
-//            }
-//            if (max < list.get(i).getAge()) {
-//                max = list.get(i).getAge();
-//            }
-//
-//            sum += list.get(i).getAge();
-//            System.out.println(list.get(i).getName());
-//            System.out.println(list.get(i).isSex());
-//            System.out.println(list.get(i).getAge());
-//        }
-//        average = sum / list.size();
-//        System.out.println(average);
-//        System.out.println(min);
-//        System.out.println(max);
-        //ctrl + alt + F
-
-        Student student = new Student();
-        student.setName("曾鹏");
-        student.setAge(20);
-        student.setSex("男");
-        student.setStuNum("2018");
-        student.setGrade("大一");
-        student.setScore(80.5);
-        System.out.println(student.getName() + "\n" + student.getAge() + "\n" + student.getSex()
-                + "\n" + student.getStuNum() + "\n" + student.getGrade() + "\n" + student.getScore());
+        for (int i = 0; i <list.size() ; i++) {
+            Student student = list.get(i);
+            System.out.println(student.getName()+student.getScore()+student.getStuNum()+student.getGrade()+student.getSex());
+        }
+        System.out.println(getMaxScore(list) + "\n" + getMinScore(list) + "\n" + getAverageScore(list));
     }
 
-    public double getMaxScore(List list)  //最高成绩
+
+    public static double getMaxScore(List<Student> list)  //最高成绩
     {
-        for(int i=0;i<list.size;i++)
+        for(int i=0;i<list.size();i++)
         {
             if (max < list.get(i).getScore())
             {
                max = list.get(i).getScore();
-               System.out.println(i);
             }
         }
-            return 0;
+        return max;
 
     }
 
-    public double getMinScore(List list)  //最低成绩
+    public static double getMinScore(List<Student> list)  //最低成绩
     {
-        for(int i=0;i<list.size;i++)
+        for(int i=0;i<list.size();i++)
         {
-            if (min < list.get(i).getScore())
+            if (min > list.get(i).getScore())
             {
                 min = list.get(i).getScore();
-                System.out.println(i);
             }
         }
-        return 0;
+        return min;
     }
 
-    public double getAverageScore(List list)//平均值
+    public static double getAverageScore(List<Student> list)//平均值
     {
-        sum+=list.get(i).getScore();
-        average = sum / list.size();
-        System.out.println(i);
-        return 0;
+        for (int i = 0; i < list.size(); i++){
+            sum += list.get(i).getScore();
+            average = sum / list.size();
+        }
+        return average;
     }
 }
 
